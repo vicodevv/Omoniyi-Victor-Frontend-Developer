2a3ce3f6760eeabf908849c926a644207dee5b38
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-12-05 03:28:20 +0100

    Deployment: Error Fix

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className="pt-[6%] grid grid-rows-[max-content,1fr,max-content] items-center">
           <div className="lg:ml-[5vw] max-lg:p-[7%] pt-[12.4%] max-lg:text-center max-w-[800px] max-lg:mx-auto">
             <h1 className="text-4xl md:text-5xl xl:text-6xl leading-[1.1] font-extrabold font-CustomFont sans-serif">
-            SpaceX: Elevating Humanity Beyond Earth's Limits
+            Elevating Humanity Beyond Earth's Limits
             </h1>
             <p className="text-base font-normal max-sm:my-12 my-8 lg:max-w-[500px] font-CustomFont sans-serif leading-8" data-animation='paragraph'>
             At SpaceX, we pioneer cutting-edge space technology since 2002, aiming to redefine the possibilities of interplanetary life. Join us in the quest to make living on other planets a reality.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/header";
-import Banner from "../components/banner";
+import Header from "components/header";
+import Banner from "components/banner";
 import Search from "components/search";
 
 const Home = () => {    
